e3c6a62224933a5e2b84a8f1f89b20341cf7be53
Abdelgawad-Ashraf <141688523+Abdelgawad-Ashraf@users.noreply.github.com>
2023-08-08 15:24:37 +0300

    Add files via upload

---
+++ First Project .py
@@ -0,0 +1,12 @@
+print ("Welcome To The Youtube Channel Name Generator: ")
+name = input ("What is your nickname? \n")
+topic = input ("What is your Channel about? \n")
+print (f"You Could Name Your Channel \"{topic} With {name}\" ")
+
+
+
+
+# print ("Welcome To The Youtube Channel Name Generator: ")
+# name = input ("What is your name? \n")
+# topic = input ("What is your channel about? \n")
+# print ("You Could Name Your Channel " + "(" + topic + " With " + name + ")")
